e58b51db1c6fcf60c51c096dcd82efd4bfb1fcf6
Briant <bmaldona@cenam.mx>
2023-08-23 11:55:56 -0600

    3d

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { SharedModule } from './shared/shared.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { GifsModule } from './gifs/gifs.module';
@@ -12,7 +13,8 @@ import { GifsModule } from './gifs/gifs.module';
   imports: [
     BrowserModule,
     GifsModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/gifs/services/gift.service.ts
+++ src/app/gifs/services/gift.service.ts
@@ -1,11 +1,15 @@
+import { Gif, SearchResponse } from './../IGisf.interface';
+import { HttpClient,HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
-  private _tagHistory:string[]=[]
-  private apikey:string=""
+  public giftsList :Gif[] = [];
 
-  constructor() { }
+  private _tagHistory:string[]=[]
+  private apikey:string="pvfszyCpuveEu0AJjGdymyneLG7ujHgf";
+  private serviceURL : string ="http://api.giphy.com/v1/gifs/";
+  constructor(private http: HttpClient) { }
 
   get tagsHistory(){
     return [...this._tagHistory];
@@ -24,9 +28,23 @@ export class GifsService {
 
   }
 
-  serachTag(tag:string){
+  // async serachTag(tag:string):Promise<void> {
+  async serachTag(tag:string) {
     if (tag.length === 0) return;
-    this.organizeHistory(tag)
+    this.organizeHistory(tag);
+    const params = new HttpParams()
+    .set("api_key",this.apikey)
+    .set("limit",10)
+    .set("q",tag)
+
+    this.http.get<SearchResponse>(`${this.serviceURL}search`,{params})
+      .subscribe(res=>{
+        this.giftsList = res.data;
+        console.log({gif: this.giftsList})
+      })
+    // fetch("http://api.giphy.com/v1/gifs/search?api_key=pvfszyCpuveEu0AJjGdymyneLG7ujHgf&q=valorant&limit=10")
+    // .then(resp => resp.json())
+    // .then(data => console.log(data))
   }
 
 
