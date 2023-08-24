128434f41d3452b7112a899d8f97dfa587ba13fb
Briant <bmaldona@cenam.mx>
2023-08-24 17:07:33 -0600

    dd

--- src/app/gifs/services/gift.service.ts
+++ src/app/gifs/services/gift.service.ts
@@ -1,61 +1,63 @@
 import { Gif, SearchResponse } from './../IGisf.interface';
-import { HttpClient,HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class GifsService {
-  public giftsList :Gif[] = [];
 
-  private _tagHistory:string[]=[]
-  private apikey:string="pvfszyCpuveEu0AJjGdymyneLG7ujHgf";
-  private serviceURL : string ="http://api.giphy.com/v1/gifs/";
-    constructor(private http: HttpClient) {
-      this.loadLocalStorage();
-      console.log(this._tagHistory[0]);
 
-   }
+  public giftsList: Gif[] = [];
+  private _tagHistory: string[] = []
+  private apikey: string = "pvfszyCpuveEu0AJjGdymyneLG7ujHgf";
+  private serviceURL: string = "http://api.giphy.com/v1/gifs/";
 
-  get tagsHistory(){
+
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+    console.log(this._tagHistory[0]);
+
+  }
+
+  get tagsHistory() {
     return [...this._tagHistory];
   }
 
-  private organizeHistory(tag:string){
+  private organizeHistory(tag: string) {
     tag = tag.toLowerCase();
 
-    if(this._tagHistory.includes(tag))
-    {
-      this._tagHistory = this._tagHistory.filter((oldTag) => oldTag !==tag );
+    if (this._tagHistory.includes(tag)) {
+      this._tagHistory = this._tagHistory.filter((oldTag) => oldTag !== tag);
     }
 
     this._tagHistory.unshift(tag);
-    this._tagHistory = this._tagHistory.splice(0,10)
+    this._tagHistory = this._tagHistory.splice(0, 10)
     this.saveLocalStorage();
   }
 
-  private saveLocalStorage():void{
-    localStorage.setItem("history",JSON.stringify(this._tagHistory));
+  private saveLocalStorage(): void {
+    localStorage.setItem("history", JSON.stringify(this._tagHistory));
   }
-  private loadLocalStorage(){
+  private loadLocalStorage() {
     if (!localStorage.getItem("history")) return;
 
-    this._tagHistory = JSON.parse(localStorage.getItem("history")!) ;
-    if (this._tagHistory.length ===0) return;
+    this._tagHistory = JSON.parse(localStorage.getItem("history")!);
+    if (this._tagHistory.length === 0) return;
 
     this.serachTag(this.tagsHistory[0]);
   }
   // async serachTag(tag:string):Promise<void> {
-  async serachTag(tag:string) {
+  async serachTag(tag: string) {
     if (tag.length === 0) return;
     this.organizeHistory(tag);
     const params = new HttpParams()
-    .set("api_key",this.apikey)
-    .set("limit",10)
-    .set("q",tag)
+      .set("api_key", this.apikey)
+      .set("limit", 10)
+      .set("q", tag)
 
-    this.http.get<SearchResponse>(`${this.serviceURL}search`,{params})
-      .subscribe(res=>{
+    this.http.get<SearchResponse>(`${this.serviceURL}search`, { params })
+      .subscribe(res => {
         this.giftsList = res.data;
-        console.log({gif: this.giftsList})
+        console.log({ gif: this.giftsList })
       })
     // fetch("http://api.giphy.com/v1/gifs/search?api_key=pvfszyCpuveEu0AJjGdymyneLG7ujHgf&q=valorant&limit=10")
     // .then(resp => resp.json())
