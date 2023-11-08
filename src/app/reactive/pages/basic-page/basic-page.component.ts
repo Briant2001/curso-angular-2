6168b98db5f0a9c907fd39cc5d4cd91e4d1258d8
Briant Maldonado Macías <briant.macias2001@gmail.com>
2023-11-07 23:24:54 -0600

    ..

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators,FormBuilder } from '@angular/forms';
 
 
@@ -7,7 +7,7 @@ import { FormGroup, Validators,FormBuilder } from '@angular/forms';
   styles: [
   ]
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit{
 
   // public myForm:FormGroup =  new FormGroup({
   //   name: new FormControl(''),
@@ -16,16 +16,46 @@ export class BasicPageComponent {
   // })
 
   public myForm:FormGroup = this.fb.group({
-    name: ['',[Validators.required, Validators.maxLength(3)]],
+    name: ['',[Validators.required, Validators.minLength(3)]],
     price:[0, [Validators.required, Validators.min(0)]],
     inStorage:[0, [Validators.required, Validators.min(0)]],
   })
-
+ 
   constructor(private fb:FormBuilder){}
-
-  onSave(){
-    console.log(this.myForm.value)
+  ngOnInit(): void {
+    // this.myForm.reset({name:"RTX",price:1000, inStorage:6})
   }
 
+  isValidField(field:string):boolean | null{
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched
+  }
+  getFieldError(field:string):string | null {
+    if(!this.myForm.controls[field]){
+      return null;
+    }
+
+    const errors = this.myForm.controls[field].errors || {}
+
+    for(const key of Object.keys(errors)){
+      switch(key){
+        case 'required': return "Este campo es requerido";
+        case 'minlength': return `Mínimo ${errors['minlength'].requiredLength} caracteres.`
+      }
+    }
+    return null;
+
+  }
+
+  onSave(){
+
+    if(this.myForm.invalid){ 
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    this.myForm.reset({name:"",price:0, inStorage:0})
+  
+  }
 
 }
+
